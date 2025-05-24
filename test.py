08a6168b6101bed07f45db6920ec049e04130d24
Dev41 <dev41@example.com>
2025-05-24 13:16:46 +0530

    Corrected the syntax issue

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 print("Hello this is the basic project")
 print("I am going to play cricker and what you gonna do ?")
-prin("This weekened i am going to play cricket")
+print("This weekened i am going to play cricket")
