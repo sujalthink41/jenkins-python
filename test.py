f241f5463d8c86386ad4b9bd9a7b82619965effd
Dev41 <dev41@example.com>
2025-05-24 13:13:40 +0530

    intentionally making mistake

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 print("Hello this is the basic project")
 print("I am going to play cricker and what you gonna do ?")
-print("This weekened i am going to play cricket")
+prin("This weekened i am going to play cricket")
